15cb7fe639991435e737a37a638dadf0c1250cc6
Eason Yi <11719668@qq.com>
2017-05-15 23:21:24 +0800

    fix

---
+++ new.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Author: Eason Yi
+# Date: $(date +'2017-05-15')
+
+function checkFilename(){
+  local f=$1
+  while true; do
+    if [[ -z $f ]]; then
+      read -p "select a file name[q/Q exit]: " f
+    elif [[ -s $f ]]; then
+      read -p "$f file not empty, select a new name[q/Q exit]: " f
+    else
+      break
+    fi
+    if [[ $f =~ [q|Q] ]]; then
+      exit 1
+    fi
+  done
+  eval "$2='$f'"
+}
+filename=''
+checkFilename "$1" filename
+
+awk 'NR < 3' $0 > $filename
+echo "# Date: $(date +'%Y-%m-%d')" >> $filename
+echo "" >> $filename
+echo "" >> $filename
+chmod u+x $filename
+exec vim $filename
