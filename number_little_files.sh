b856bfda6743be704919806c66efb860695ea863
Eason Yi <11719668@qq.com>
2017-05-16 22:42:20 +0800

    fix

---
+++ number_little_files.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Author: Eason Yi
+# Date: 2017-05-16
+
+filemaxsize="20k"
+if [[ $1 =~ ^[0-9]+[k|K]$ ]]; then
+  filemaxsize=${1/K/k}
+fi
+echo "File max size: $filemaxsize"
+i=0
+for f in $(find . -maxdepth 1 -type f -size -${filemaxsize}); do
+  echo $((++i)): $f;
+  nl $f;
+done
+echo "File max size: $filemaxsize, Matched files: $i"
