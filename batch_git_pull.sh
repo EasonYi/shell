58d3ef79b7e04e51f12849e1f86e097797fc84f3
of1972 <yiyongjian@qianmi.com>
2017-05-17 17:07:41 +0800

    fix

---
+++ batch_git_pull.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Author: Eason Yi
+# Date: 2017-05-17
+
+function dir(){
+  for i in $(ls); do 
+    if [[ -d $i ]]; then 
+      echo $i
+    fi
+  done
+}
+
+i=1
+t=$(dir|wc -l)
+for d in $(dir); do 
+  echo "[INFO] Update ... $d ... ($i /${t#     })"
+  let i++
+  cd $d
+  git pull
+  cd ..
+done
+
