34fe6eae356f86286ab3dbc49cc2bbf3160f8279
of1972 <yiyongjian@qianmi.com>
2017-05-24 12:08:29 +0800

    fix

--- show_cmd_detail.sh
+++ show_cmd_detail.sh
@@ -8,14 +8,14 @@ if [[ -z $1 ]]; then
 fi
 
 cmd_name=$1
-cmd_path=$(which $cmd_name)
-cmd_true_path=$(ls -lh $cmd_path)
-cmd_size=$(echo $cmd_true_path|awk '{print $5}')
-cmd_file_type=$(file $cmd_path)
+cmd_path=$(which "$cmd_name")
+cmd_true_path=$(ls -lh "$cmd_path")
+cmd_size=$(echo "$cmd_true_path"|awk '{print $5}')
+cmd_file_type=$(file "$cmd_path")
 
 if [[ -z $(echo $cmd_file_type|grep text) ]]; then
   echo "It's a binary command"
-elif [[ -n $(find $(dirname $cmd_path) -name $(basename $cmd_path) -size -20k) ]]; then
+elif [[ -n $(find "$(dirname "$cmd_path")" -name "$(basename "$cmd_path")" -size -20k) ]]; then
   cat "$cmd_path"
 else
   head -200 "$cmd_path"
