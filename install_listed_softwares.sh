450142627af2a72b5c37111ec2977acd115c718c
of1972 <yiyongjian@qianmi.com>
2018-04-09 10:05:36 +0800

    fix

--- install_listed_softwares.sh
+++ install_listed_softwares.sh
@@ -2,6 +2,10 @@
 # Author: Eason Yi
 # Date: 2018-04-09
 
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew tap caskroom/cask
+
+
 brew_softwares_file='./brew/brew.list'
 for i in $(cat ${brew_softwares_file}); do brew install $i; done
 brew cleanup
